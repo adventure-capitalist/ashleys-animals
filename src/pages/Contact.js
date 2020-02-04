54f80103b42e231316fd0a1969652f73f1b24c43
Ashley Mikkola <ashleylaurel@outlook.com>
2020-02-04 12:26:05 +0000

    still trying to get the form to work

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,19 +11,24 @@ const Contact = props => (
           data-netlify="true"
           data-netlify-honeypot="bot-field"
         >
-          <input type="hidden" name="form-name" value="contact" />
+          <input type="hidden" name="bot-field" />
           <div className="fields">
             <div className="field half">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
+              <input type="text" name="name" id="name" required />
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="text" name="email" id="email" required />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="6"></textarea>
+              <textarea
+                name="message"
+                id="message"
+                rows="6"
+                required
+              ></textarea>
             </div>
           </div>
           <ul className="actions">
