50d7506e08fb022d6f1ed7f033efdd6ba9d7d17e
Ashley Mikkola <ashleylaurel@outlook.com>
2020-02-04 14:30:04 +0000

    adjusting the CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,22 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/admin`,
+        name: "img",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,44 +4,69 @@ import Contact from "./Contact"
 import Footer from "./Footer"
 import Main from "./Main"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 if (typeof window !== "undefined") {
   // eslint-disable-next-line global-require
   require("smooth-scroll")('a[href*="#"]')
 }
 
-export default () => (
-  <main className="is-preload">
-    <div id="wrapper">
-      {/* Menu */}
-      <header id="header" className="alt">
-        <Link to="/" className="logo">
-          <strong>Ashley's</strong> <span>Animal Adoption</span>
-        </Link>
-        <nav>
-          <a href="/Menu">Menu</a>
-        </nav>
-      </header>
+const Index = props => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  return (
+    <main className="is-preload">
+      <div id="wrapper">
+        {/* Menu */}
+        <header id="header" className="alt">
+          <Link to="/" className="logo">
+            <h1>{data.title}</h1>
+            <strong>Ashley's</strong> <span>Animal Adoption</span>
+          </Link>
+          <nav>
+            <a href="/Menu">Menu</a>
+          </nav>
+        </header>
 
-      {/* Banner */}
-      <Banner />
+        {/* Banner */}
+        <Banner />
 
-      {/* Main */}
-      <Main />
-      {/* Contact */}
-      <Contact />
+        {/* Main */}
+        <Main />
+        {/* Contact */}
+        <Contact />
 
-      {/* Footer */}
-      <Footer />
+        {/* Footer */}
+        <Footer />
 
-      {/* Scripts */}
-      <script src="assets/js/jquery.min.js"></script>
-      <script src="assets/js/jquery.scrolly.min.js"></script>
-      <script src="assets/js/jquery.scrollex.min.js"></script>
-      <script src="assets/js/browser.min.js"></script>
-      <script src="assets/js/breakpoints.min.js"></script>
-      <script src="assets/js/util.js"></script>
-      <script src="assets/js/main.js"></script>
-    </div>
-  </main>
-)
+        {/* Scripts */}
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/jquery.scrolly.min.js"></script>
+        <script src="assets/js/jquery.scrollex.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+      </div>
+    </main>
+  )
+}
+export default Index
+
+export const query = graphql`
+  query {
+    allFile(
+      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              image
+            }
+          }
+        }
+      }
+    }
+  }
+`
